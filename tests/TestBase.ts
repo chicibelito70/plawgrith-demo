36ea6efa19798cb737f6c0707e08e827415ea4db
chicibelito70 <chicibelito07@gmail.com>
2025-11-22 20:20:58 -0400

    first commit

---
+++ tests/TestBase.ts
@@ -0,0 +1,17 @@
+
+import { test as driver, type Expect } from '@playwright/test';
+import { LoginPage } from '@pages/LoginPage';
+import { AddSystemUserPage } from '@pages/AddSystemUserPage';
+
+const test = driver.extend<{
+	loginPage: LoginPage
+	expect: Expect
+	addUserPage: AddSystemUserPage
+}>({
+	expect: async ({}, use) => await use(driver.expect),
+	loginPage: async ({ page }, use) => await use(new LoginPage(page)),
+	addUserPage: async ({ page }, use) => await use(new AddSystemUserPage(page))
+});
+
+export { test };
+export const expect = test.expect;
